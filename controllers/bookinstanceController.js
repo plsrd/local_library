dec73baa8c017394c21e3340cdb8ce848787b838
Racheal Pennell <racheal.larimer@gmail.com>
2022-06-15 20:05:20 -0700

    add bookinstance form

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,7 @@
 const BookInstance = require('../models/bookinstance');
+const Book = require('../models/book');
+
+const { body, validationResult } = require('express-validator');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function (req, res, next) {
@@ -33,14 +36,62 @@ exports.bookinstance_detail = function (req, res, next) {
 };
 
 // Display BookInstance create form on GET.
-exports.bookinstance_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance create GET');
+exports.bookinstance_create_get = function (req, res, next) {
+  Book.find({}, 'title').exec((err, book_list) => {
+    if (err) return next(err);
+
+    res.render('bookinstance_form', {
+      title: 'Create Book Instance',
+      book_list,
+    });
+  });
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status').escape(),
+  body('due_back', 'Invalid date')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    let bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      Book.find({}, 'title').exec((err, book_list) => {
+        if (err) return next(err);
+
+        res.render('bookinstance_form', {
+          title: 'Create Book Instance',
+          book_list,
+          selected_book: bookinstance.book._id,
+          bookinstance,
+          errors: errors.array(),
+        });
+
+        return;
+      });
+    } else {
+      bookinstance.save(err => {
+        if (err) return next(err);
+
+        res.redirect(bookinstance.url);
+      });
+    }
+  },
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function (req, res) {
